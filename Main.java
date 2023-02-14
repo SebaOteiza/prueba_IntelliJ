77880bd9b83aa086e8bccf1a8fefb6109cdf7a50
Sebastian Oteiza <oteiza.sebastian@gmail.com>
2023-02-14 13:02:52 -0300

    tipos de variables, declaraciones

--- Main.java
+++ Main.java
@@ -3,6 +3,54 @@ public class Main { //public = publico, class = distintas clases que puede tener
 
     public static void main(String[] args) {
 
-        System.out.println("Hola loco desde IntellIJ");
+        //TIPOS DE VARIABLES
+
+        //Primitivas : Int, short, byte, long, boolean, float, double
+        //guardan estos datos, caracteres, números decimales, etc
+
+        //EJEMPLOS DE VARIABLES, cómo se declaran y para qué sirven
+
+        int edad = 34;
+        long a = 45454555;
+        double b = 4.55, c=5.5;
+
+        String nombre = "seba";
+        System.out.println(edad);
+        System.out.println(nombre);
+        System.out.println(a);
+        System.out.println("Mi nombre es: "+nombre);
+
+
+
+
+
+
+        //Referenciales a Objetos: String, arreglos, etc.
+        //los string son cadenas de caracteres, lo que un hace son referenciales a variables primitivas de tipo short
+        //arreglos, conjuntos de elementos que almacenan info de variables primitivas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //main significa principal, toodo lo programado se ejecuta desde aquí
+        //PSVM con esas siglas creo el método main más rápido
+        //MAIN palabra reservada, en color naranja.
+
+       // System.out.println("Hola loco desde IntellIJ");
+        //sout + ctrl espacio
+        //System.out.println("IDE que se parece a eclipse o NetBeans");
     }
 }
