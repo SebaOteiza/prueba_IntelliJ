0958b53207aef45af47aa0b53b36aedeea83db3b
Sebastian Oteiza <oteiza.sebastian@gmail.com>
2023-02-14 12:39:40 -0300

    probando intellij

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main { //public = publico, class = distintas clases que puede tener un método en java
+    //de publico, cualquier archivo de SRC pueden acceder a este método en esta clase
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola loco desde IntellIJ");
+    }
+}
